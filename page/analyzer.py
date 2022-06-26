8a38bc9b56d89ddf21934ccf739fa906f0ac0933
walan566 <68682810+walan566@users.noreply.github.com>
2022-06-27 00:44:31 +0800

    20220627

--- page/analyzer.py
+++ page/analyzer.py
@@ -2,26 +2,17 @@ import streamlit as st
 import requests
 from streamlit_lottie import st_lottie_spinner
 import streamlit.components.v1 as components
-from crypto import ada, bit, eth, xlm, ltc, usdt, dot, bch, doge, shib
-
-
-
-#https://github.com/Japan745/CryptoBase
-## Technical indicators to be used
-# ADX, BBANDS , CCI, EMA, ICHIMOKU, MACD, MOM, PERCENT_B, RSI , SMA, STOCH, STOCHRSI, ULTOSC, VWAP, WILLR, WMA
-# https://github.com/ho-steven/SP500-Analyzer/blob/main/apps/prediction.py
-
-
-# Investing: RSI(14), STOCH(9,6), STOCHRSI(14), MACD(12,26), ADX(14), Williams %R, CCI(14), ATR(14), Highs/Lows(14), Ultimate Oscillator, ROC, Bull/Bear Power(13), (SMA & EMA) MA5, MA10, MA20, MA50, MA100, MA200
-# https://www.investing.com/crypto/bitcoin/btc-usd-technical?cid=1035793
-# https://investpy.readthedocs.io/_info/usage.html
-# https://finviz.com/crypto_charts.ashx?t=BTCUSD
-
-
-
-# Tradingview: RSI(14), Stochastic %K (14, 3, 3), CCI(20), ADX(14), Awesome Oscillator, Momentum (10), MACD Level (12, 26), Stochastic RSI Fast (3, 3, 14, 14), Williams Percent Range (14), Bull Bear Power, Ultimate Oscillator (7, 14, 28), Ichimoku Base Line (9, 26, 52, 26), Volume Weighted Moving Average (20), Hull Moving Average (9)
-#https://tradingview.brianthe.dev/
-#https://www.tradingview.com/symbols/BTCUSD/technicals/
+from pytrends.request import TrendReq
+from pytrends import dailydata
+import yfinance as yf
+from datetime import datetime, date
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from wordcloud import WordCloud
+from PIL import Image
+from mpl_finance import candlestick_ohlc
+import matplotlib.dates as mpl_dates
 
 
 def get_analyzer():
@@ -61,8 +52,8 @@ def get_analyzer():
             get_bnbeth(height, width)
         if ticker == "ETH/BTC":
             get_ethbtc(height, width)        
-        if ticker == "SOL/BNB":
-            get_solbnb(height, width)
+        if ticker == "SOL/ETH":
+            get_soleth(height, width)
         if ticker == "SOL/BTC":
             get_solbtc(height, width)
 
@@ -107,6 +98,223 @@ def get_btcusd(height, width):
         height=height,
         width=width,
     )
+    st.markdown("***")
+
+    #Candlestick Pattern Analysis
+    st.subheader('Candlestick Pattern Analysis')
+    st.info("Candlestick Pattern Analysis")
+    from candlestick import candlestick
+
+    # Find candles where inverted hammer is detected
+
+    candles = requests.get('https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1d&limit=30')
+    candles_dict = candles.json()
+
+    candles_df = pd.DataFrame(candles_dict,
+                           columns=['open_time', 'open', 'high', 'low', 'close', 'volume','close_time', 'quote_asset_volume', 'number_of_trades','taker_buy_base_asset_volume', 'taker_buy_quote_asset_volume',
+    'ignore'])
+
+    candles_df['open_time'] = pd.to_datetime(candles_df['open_time'], unit='ms')
+    candles_df['close_time'] = pd.to_datetime(candles_df['close_time'], unit='ms')
+
+    candles_df = candlestick.inverted_hammer(candles_df, target='InvertedHammers')
+    candles_df = candlestick.doji_star(candles_df, target='doji_star')
+    candles_df = candlestick.bearish_harami(candles_df, target='bearish_harami')
+    candles_df = candlestick.bullish_harami(candles_df, target='bullish_harami')
+    candles_df = candlestick.dark_cloud_cover(candles_df, target='dark_cloud_cover')
+    candles_df = candlestick.doji(candles_df, target='doji')
+    candles_df = candlestick.dragonfly_doji(candles_df, target='dragonfly_doji')
+    candles_df = candlestick.hanging_man(candles_df, target='hanging_man')
+    candles_df = candlestick.gravestone_doji(candles_df, target='gravestone_doji')
+    candles_df = candlestick.bearish_engulfing(candles_df, target='bearish_engulfing')
+    candles_df = candlestick.bullish_engulfing(candles_df, target='bullish_engulfing')
+    candles_df = candlestick.hammer(candles_df, target='hammer')
+    candles_df = candlestick.morning_star(candles_df, target='morning_star')
+    candles_df = candlestick.morning_star_doji(candles_df, target='morning_star_doji')
+    candles_df = candlestick.piercing_pattern(candles_df, target='piercing_pattern')
+    candles_df = candlestick.rain_drop(candles_df, target='rain_drop')
+    candles_df = candlestick.rain_drop_doji(candles_df, target='rain_drop_doji')
+    candles_df = candlestick.star(candles_df, target='star')
+    candles_df = candlestick.shooting_star(candles_df, target='shooting_star')
+
+    st.dataframe(candles_df)
+    st.markdown("***")
+
+    #Google Trend
+    st.subheader('Google Trend Analysis')
+    st.info("Google Trends values are calculated on a scale from 0 to 100. A higher value indicates the search item is more popular. A value of 0 indicates there was not enough data for the search items.")
+
+    with st.expander(" 👁  Click to view the recent 7-days Google trend for Bitcoin"):  
+        pytrends = TrendReq(hl='en-US', tz=360)
+        kw_list = ["Bitcoin","BTC"]
+        pytrends.build_payload(kw_list, cat=0, timeframe='now 7-d', geo='', gprop='')
+        df_pytrends = pytrends.interest_over_time()
+        st.dataframe(df_pytrends)
+
+
+    with st.expander(" 👁  Click to view the correlation of recent 30 days price and Google trend"):
+        ticker = 'BTC-USD' # which stock to search for
+        kw_list = 'Bitcoin'
+        t_now = datetime.now()
+        t_prev= datetime.fromtimestamp(t_now.timestamp()-(3600*24*30))
+        trends = dailydata.get_daily_data(kw_list,t_prev.year,t_prev.month,t_now.year,t_now.month, geo='')
+        fin_data = yf.download(ticker,start=t_prev.strftime('%Y-%m-%d'),end=t_now.strftime('%Y-%m-%d'))
+
+        fin_indx = 0 #Prices: 0=Open,1=High,2=Low,3=Close,4=Adj Close,5=Volume
+        trend_indx = 3 #Popularity: 0=unscaled 0-100, 1=monthly, 2=isPartial, 3=scaled 
+
+        plt.style.use('ggplot')
+        fig,ax = plt.subplots(figsize=(14,9))
+
+        fin_keys = fin_data.keys() # keys for naming plotted finance data
+        trend_keys = trends.keys() # keys for naming plotted trends data
+
+        fin_x = [ii.timestamp() for ii in fin_data.index] # formatting dates into timestamp for plotting
+        fin_y = (fin_data.values)[:,fin_indx] # trend data to plot
+
+        trend_x = [ii.timestamp() for ii in trends.index] # formatting dates into timestamp for plotting
+        trend_y = (trends.values)[:,trend_indx] # trend data to plot
+
+        trend_start_indx = np.argmin(np.abs(np.subtract(trend_x,fin_x[0])))
+        trend_end_indx   = np.argmin(np.abs(np.subtract(trend_x,fin_x[-1])))
+        trend_x = trend_x[trend_start_indx:trend_end_indx+1] # align trends + stock $
+        trend_y = trend_y[trend_start_indx:trend_end_indx+1] # align trends + stock $
+
+        scat1 = ax.scatter(trend_x,trend_y,color=plt.cm.tab20(0),s=120) # scatter trend data
+
+        ax.set_ylabel('Trend: '+(trend_keys[trend_indx].replace('_',' ')),
+                        fontsize=20,color=plt.cm.tab20(0))
+        x_ticks = ax.get_xticks()
+        x_str_labels = [(datetime.fromtimestamp(ii)).strftime('%m-%d-%Y') for ii in x_ticks]
+        ax.set_xticklabels(x_str_labels) # format dates on x-axis
+        ax.set_xlabel('Time [Month-Day-Year]',fontsize=20)
+
+        ax2 = ax.twinx() # twin axis to plot both data on the same plot
+        ax2.grid(False) # this prevents the axes from being too messy
+        scat2 = ax2.scatter(fin_x,fin_y,color=plt.cm.tab20(2),s=120) # scatter finance data
+        ax2.set_ylabel(fin_keys[fin_indx]+' Price [$ USD]',fontsize=20,color=plt.cm.tab20(2))
+
+        plt.title(ticker,fontsize=20)
+        plt.savefig('Images/btc_gtrend.png')
+        from PIL import Image
+        image_scaled = Image.open('Images/btc_gtrend.png')
+        st.image(image_scaled)
+
+
+    with st.expander(" 👁  Click to view Wordcloud of related Google Trend Search Key Words"):
+        pytrend = TrendReq(hl='en-US', tz=-480)
+        kw_list = ["bitcoin"]
+        pytrend.build_payload(kw_list, cat=0, timeframe='now 7-d', geo='US', gprop='')
+        trends = pytrend.related_queries()
+        df_sg = trends["bitcoin"]["top"]
+        text = ' '.join(df_sg["query"].to_list())
+        # Generate a word cloud image
+        wordcloud = WordCloud().generate(text)
+
+        # Display the generated image:
+        # the matplotlib way:
+        plt.figure()
+        plt.imshow(wordcloud, interpolation='bilinear')
+        plt.axis("off")
+
+        plt.title(ticker,fontsize=20)
+        plt.savefig('Images/btc_wordcloud.png')
+        from PIL import Image
+        image_wc = Image.open('Images/btc_wordcloud.png')
+        st.image(image_wc)
+
+    st.markdown("***")
+    #Support and Resistance
+    st.subheader('Support and Resistance Analysis')
+    st.info("Support occurs where a downtrend is expected to pause due to a concentration of demand. Resistance occurs where an uptrend is expected to pause temporarily, due to a concentration of supply. ")   
+
+    plt.rcParams['figure.figsize'] = [12, 7]
+
+    plt.rc('font', size=14) 
+    fin_data['Date'] = pd.to_datetime(fin_data.index)
+    fin_data['Date'] = fin_data['Date'].apply(mpl_dates.date2num)
+
+    fin_data = fin_data.loc[:,['Date', 'Open', 'High', 'Low', 'Close']]
+
+    def isSupport(fin_data,i):
+        support = fin_data['Low'][i] < fin_data['Low'][i-1]  and fin_data['Low'][i] < fin_data['Low'][i+1] \
+        and fin_data['Low'][i+1] < fin_data['Low'][i+2] and fin_data['Low'][i-1] < fin_data['Low'][i-2]
+
+        return support
+
+    def isResistance(fin_data,i):
+        resistance = fin_data['High'][i] > fin_data['High'][i-1]  and fin_data['High'][i] > fin_data['High'][i+1] \
+        and fin_data['High'][i+1] > fin_data['High'][i+2] and fin_data['High'][i-1] > fin_data['High'][i-2] 
+
+        return resistance
+    levels = []
+    for i in range(2,fin_data.shape[0]-2):
+        if isSupport(fin_data,i):
+            levels.append((i,fin_data['Low'][i]))
+        elif isResistance(fin_data,i):
+            levels.append((i,fin_data['High'][i]))
+
+    def plot_all():
+        fig, ax = plt.subplots()
+
+        candlestick_ohlc(ax,fin_data.values,width=0.6, \
+                        colorup='green', colordown='red', alpha=0.8)
+
+        date_format = mpl_dates.DateFormatter('%d %b %Y')
+        ax.xaxis.set_major_formatter(date_format)
+        fig.autofmt_xdate()
+
+        fig.tight_layout()
+
+        for level in levels:
+            plt.hlines(level[1],xmin=fin_data['Date'][level[0]],\
+                    xmax=max(fin_data['Date']),colors='blue')
+        plt.savefig('Images/support_resist_btc.png')
+        from PIL import Image
+        image_sr = Image.open('Images/support_resist_btc.png')
+        st.image(image_sr)
+    plot_all()
+
+    #Fibonacci Retracements
+    st.subheader('Fibonacci Retracements')
+    st.info("Fibonacci retracement levels are horizontal lines that indicate where support and resistance are likely to occur. Each level is associated with a percentage. The percentage is how much of a prior move the price has retraced.")   
+    highest_swing = -1
+    lowest_swing = -1
+    for i in range(1,fin_data.shape[0]-1):
+        if fin_data['High'][i] > fin_data['High'][i-1] and fin_data['High'][i] > fin_data['High'][i+1] and (highest_swing == -1 or fin_data['High'][i] > fin_data['High'][highest_swing]):
+            highest_swing = i
+
+        if fin_data['Low'][i] < fin_data['Low'][i-1] and fin_data['Low'][i] < fin_data['Low'][i+1] and (lowest_swing == -1 or fin_data['Low'][i] < fin_data['Low'][lowest_swing]):
+            lowest_swing = i
+    ratios = [0,0.236, 0.382, 0.5 , 0.618, 0.786,1]
+    colors = ["black","r","g","b","cyan","magenta","yellow"]
+    levels = []
+
+    max_level = fin_data['High'][highest_swing]
+    min_level = fin_data['Low'][lowest_swing]
+
+    for ratio in ratios:
+        if highest_swing > lowest_swing: # Uptrend
+            levels.append(max_level - (max_level-min_level)*ratio)
+        else: # Downtrend
+            levels.append(min_level + (max_level-min_level)*ratio)
+
+    plt.rcParams['figure.figsize'] = [12, 7]
+
+    plt.rc('font', size=14)
+
+    plt.plot(fin_data['Close'])
+    start_date = fin_data.index[min(highest_swing,lowest_swing)]
+    end_date = fin_data.index[max(highest_swing,lowest_swing)]
+    for i in range(len(levels)):
+        plt.hlines(levels[i],start_date, end_date,label="{:.1f}%".format(ratios[i]*100),colors=colors[i], linestyles="dashed")
+    plt.legend()
+
+    plt.savefig('Images/fibonacci_btc.png')
+    from PIL import Image
+    image_fr = Image.open('Images/fibonacci_btc.png')
+    st.image(image_fr)
+
 
 def get_ethusd(height, width):
 
@@ -351,7 +559,7 @@ def get_ethbtc(height, width):
     )
 
 
-def get_solbnb(height, width):
+def get_soleth(height, width):
 
     components.html(
         f"""
@@ -365,7 +573,7 @@ def get_solbnb(height, width):
         {{
         "width": {width},
         "height": {height},
-        "symbol": "BINANCE:SOLBNB",
+        "symbol": "BINANCE:SOLETH",
         "interval": "D",
         "timezone": "Asia/Hong_Kong",
         "theme": "light",
@@ -431,148 +639,3 @@ def get_solbtc(height, width):
         width=width,
     )
 
-
-# https://assets10.lottiefiles.com/packages/lf20_8lcyef9f.json
-# https://assets1.lottiefiles.com/packages/lf20_2xgpnwrq.json
-# https://assets3.lottiefiles.com/packages/lf20_aj79ghex.json
-
-
-# Forecast
-# https://assets10.lottiefiles.com/packages/lf20_8lcyef9f.json
-
-# analyser
-# https://assets7.lottiefiles.com/packages/lf20_p9ce3saq.json
-
-# market
-# https://assets1.lottiefiles.com/packages/lf20_2xgpnwrq.json
-
-# portfolio
-# https://assets6.lottiefiles.com/private_files/lf30_gonpfxdh.json
-
-
-    # st.warning(
-        # "NOTE :-  The Predicted values of cryptocurrencies are forecasted by machine learning algorithm and are for your "
-        # "reference only, it doesn't guarantee future exact values."
-        # "Please do a research before taking any further decision based on this forecasted values.")
-
-    #  Correlation analysis
-    
-    #  Price chart (1m, 1h, 1d)
-    #  Basic infographics + intro
-    #  Technical Analysis (ta-lib)
-    
-    # AI forecasting
-    #  Trading views
-    #  News Sentiment Analysis
-    #  Social Media Analysis (lunarcrush)
-       # Twitter Analysis
-    #AI Models (logistic regression, LSTM, Random Forest)
-    #BNB/ETH, BNB/USD, BNB/BTC, BTC/USD, ETH/BTC, ETH/USD, SOL/BNB, SOL/BTC, SOL/USD, SOL/ETH (Huobi)
-
-# def user_input_features(ticker, period):
-    # today = date.today()
-    # #ticker = st.sidebar.selectbox("Ticker", ('BTC/USDT','ETH/USDT','BNB/USDT','SOL/USDT'))
-    # #period = st.sidebar.selectbox("Period", ('1d','1h','1m'))
-    # if period == '1day': 
-        # start_date = st.sidebar.date_input("Start Date", date(2020, 1, 1))
-        # end_date = st.sidebar.date_input("End Date", date.today())        
-        # return ticker, start_date, end_date
-    # elif period == '1h':
-        # no_of_hrs_analysis = st.sidebar.radio("Number of Hours for analysis", ["1000"])
-        # return ticker, no_of_hrs_analysis
-    # elif period == '1min':
-        # no_of_mins_analysis = st.sidebar.radio("Number of Minutes for analysis", ["1000"])
-        # return ticker, no_of_mins_analysis
-        
-
-# def time_series_1d(ticker, period, start, end ):
-
-    # # Initialize client - apikey parameter is requiered
-    # td = TDClient(apikey="6b27384627a5423aa7b69032d14d9ca8")
-
-    # # Construct the necessary time series (interval:1min, 5min, 15min, 30min, 45min, 1h, 2h, 4h, 8h, 1day, 1week, 1month)
-    # ts = td.time_series(
-        # symbol=ticker,
-        # interval=period,
-        # outputsize='1000',
-        # exchange='Binance', 
-        # start_date=start,
-        # end_date=end,
-        # timezone="America/New_York",
-    # )
-
-    # # Returns pandas.DataFrame
-    # time_s_1d = ts.as_pandas() 
-    # st.dataframe(time_s_1d)
-
-    # st.sidebar.header('User Input Parameters')
-    # ticker = st.sidebar.selectbox("Ticker", ('BTC/USD','ETH/USD','BNB/USD','SOL/USD','BNB/BTC','BNB/ETH','ETH/BTC', 'SOL/BNB', 'SOL/BTC'))
-    # period = st.sidebar.selectbox("Period", ('1day','1h','1min'))
-    # if period == "1day" and ticker!='SOL/ETH':
-        # symbol, start, end = user_input_features(ticker, period)
-        # start = pd.to_datetime(start)
-        # end = pd.to_datetime(end)
-        # time_series_1d(ticker, period, start, end)
-        #binance = ccxt.binance()
-        #btc_ticker = binance.fetchMarkOHLCV ("BTC/USDT", '1d')
-        #st.dataframe(btc_ticker)
-        #st.table(btc_ticker)
-        #st.json(btc_ticker)
-
-    # elif period == "1h":
-        # symbol, no_of_hrs = user_input_features(ticker, period)
-    # elif period == "1min":
-        # symbol, no_of_mins = user_input_features(ticker, period)
-
-    #ticker = st.sidebar.selectbox("Ticker", ('BTC/USDT','ETH/USDT','BNB/USDT','SOL/USDT'))
-    #period = st.sidebar.selectbox("Period", ('1d','1h','1m'))
-
-
-    #if submit_button:
-        
-        # if crypto == "Bitcoin (BTC)":
-            # with st_lottie_spinner(lottie_json):
-                # bit.get_bit()
-        # if crypto == "Ethereum (ETH)":
-            # with st_lottie_spinner(lottie_json):
-                # eth.get_eth()
-        # if crypto == "Stellar (XLM)":
-            # with st_lottie_spinner(lottie_json):
-                # xlm.get_xlm()
-        # if crypto == "Tether (USDT)":
-            # with st_lottie_spinner(lottie_json):
-                # usdt.get_usdt()
-        # if crypto == "Bitcoin Cash (BCH)":
-            # with st_lottie_spinner(lottie_json):
-                # bch.get_bch()
-        # if crypto == "Litecoin (LTC)":
-            # with st_lottie_spinner(lottie_json):
-                # ltc.get_ltc()
-        # if crypto == "Polkadot (DOT)":
-            # with st_lottie_spinner(lottie_json):
-                # dot.get_dot()
-        # if crypto == "Dogecoin (DOGE)":
-            # with st_lottie_spinner(lottie_json):
-                # doge.get_doge()
-        # if crypto == "Cardano (ADA)":
-            # with st_lottie_spinner(lottie_json):
-                # ada.get_ada()
-        # if crypto == "Shiba Inu (SHIB)":
-            # with st_lottie_spinner(lottie_json):
-                # shib.get_shib()
-        # To-do list: 
-        #Market: Coorelation matrix
-        #Analyzer: 
-        #1.Price and icon
-        #2.Chart and technical analysis ((ta-lib))
-        #3.news
-        #4.social media
-        #5.comparison with benchmark (other 3 cryptocurrency/ ncid index) (if have time)
-        
-        
-        #Forecasting and Recommendations
-        #  Trading views (& summarized recommendation)
-        #  News Sentiment Analysis (& summarized recommendation)
-        #  Social Media Analysis (lunarcrush)
-        #  Twitter Analysis
-        #  AI Models (logistic regression, LSTM, Random Forest, Reinforcement Learning)
